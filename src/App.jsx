f287b09a3702420d8b63de4f86f16106a096b836
mateozivano <mattzivano@gmail.com>
2022-07-02 23:18:19 -0300

    itemlists

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import ItemListContainer from "./componentes/container/ItemListContainer";
 import Titulo from "./componentes/container/titulo";
@@ -10,19 +10,19 @@ function App() {
   const objStyle = {color: "white", backgroundColor: "black", fontSize: 50};
 
   return (
-    
+    <section>
     <div style={objStyle} className="App">
-      <NavBar/>
-      <Titulo titulo="ron"/>
-      <ItemListContainer tituloITC="Capitan morgan" subTituloITC="1400"/>
-      <ItemListContainer tituloITC="Havanna club" subTituloITC="2300"/>
-      <ItemListContainer tituloITC="Bacardi" subTituloITC="2000"/>
-      <ItemListContainer tituloITC="Malibu" subTituloITC="1000"/>
+      <NavBar />
+      </div>
+      <Titulo titulo="Bebidas disponibles"/>
+      <ItemListContainer />
+      
       
       
 
      
-    </div>
+    
+    </section>
   );
 }
 
--- src/componentes/container/ItemListContainer.jsx
+++ src/componentes/container/ItemListContainer.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-
+import { useState, useEffect } from 'react'
+import { gFetch } from "../helpers/getFetch";
  
-const ItemListContainer = ({tituloITC, subTituloITC}) => {
+/* const ItemListContainer = ({tituloITC, subTituloITC}) => {
  
     return(
         <div>
@@ -10,7 +11,51 @@ const ItemListContainer = ({tituloITC, subTituloITC}) => {
         </div>
     )
 
+} (esto queda por las dudas)*/
+
+const ItemListContainer = ({ greeting }) => {
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+   
+    
+
+    useEffect(() => {
+        gFetch /* aca simula un llamado a una api */
+        .then(resp => setProducts(resp))
+        .catch( err => console.log(err))
+        .finally(() => setLoading(false))
+    }, [])
+
+return (
+        <div>
+           { loading ? /*aca usa ternario, una suerte de IF*/ 
+                <h2>Cargando ...</h2> 
+            : 
+                products.map(prod =>   <div key={prod.id} className='col-md-4 p-1'>                    
+                                            <div className="card w-100 mt-5" >
+                                                <div className="card-header">
+                                                    {`${prod.name} - ${prod.categoria}`}
+                                                </div>
+                                                <div className="card-body">
+                                                    <img src={prod.foto} alt='' className='w-50' />
+                                                    {prod.stock}                                                            
+                                                </div>
+                                                <div className="card-footer">                                               
+                                                    <button className="btn btn-outline-primary btn-block">
+                                                        detalle del producto
+                                                    </button>                                               
+                                                </div>
+                                            </div>                                                                                                                            
+                                        </div> ) 
+            }
+        </div>
+    )
 }
+
+
+
+
+
 
 
 export default ItemListContainer
